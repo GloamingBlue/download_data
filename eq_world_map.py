6ef491aac3711a105dd5e98a063826dded480724
GloamingBlue <1992491680@qq.com>
2022-03-31 18:19:09 +0800

    completed

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,8 +1,9 @@
 import plotly.express as px
 import json
+import pandas as pd
 
 # 探索数据的结构
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
 	all_eq_data = json.load(f)
 all_eq_dicts = all_eq_data['features']
@@ -17,15 +18,23 @@ for eq_dict in all_eq_dicts:
 	lons.append(lon)
 	lats.append(lat)
 # 绘制散点图
+data = pd.DataFrame(
+	data=list(zip(lons, lats, titles, mags)), columns=['经度', '纬度', '位置', '震级']
+)
+data.head()
 fig = px.scatter(
-	x=lons,
-	y=lats,
-	labels={'x': '经度', 'y': '纬度'},
+	data,
+	x='经度',
+	y='纬度',
 	range_x=[-200, 200],
 	range_y=[-90, 90],
 	width=800,
 	height=800,
 	title='全球地震散点图',
+	size='震级',
+	size_max=10,
+	color='震级',
+	hover_name='位置',
 )
 fig.write_html('global_earthquakes.html')
 fig.show()
---
+++ show_color_scales.py
@@ -0,0 +1,5 @@
+import plotly.express as px
+
+# 打印plotly express中其他颜色渐变的格式
+for key in px.colors.named_colorscales():
+	print(key)
